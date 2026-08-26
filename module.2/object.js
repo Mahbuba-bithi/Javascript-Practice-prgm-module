40fb87d745b9f07cd2542d50742741279d8e32de
Mahbuba Bithi <mahbubabithi2393@gmail.com>
2026-08-25 23:57:10 -0700

    object done

--- module.2/object.js
+++ module.2/object.js
@@ -4,4 +4,18 @@ let user = {
     name: "bithi",
     age: 25,
     address: "dhaka"
+};
+
+//console.log(user["name"])
+
+delete user.address
+user.address = "rajshahi"
+user.address = {
+    city: "rajshahi",
+    area:"uposhoshor"
 }
+
+
+//object er vitor theke kivabe key value alada kora jay also object theke arry
+//console.log(Object.entries(user))
+console.log(Object.keys(user))
