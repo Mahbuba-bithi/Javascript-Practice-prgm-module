58450a458586d6d765b62f4d7928bbd56fec617c
Mahbuba Bithi <mahbubabithi2393@gmail.com>
2026-08-26 15:26:09 +0600

    loop practice

---
+++ module.2/loop.js
@@ -0,0 +1,25 @@
+// loop for while
+
+// for (let i = 0; i<5; i++){
+//     console.log(`count ${i}`)
+// }
+for (let i = 5; i >= 0; i--){
+    console.log(`count ${i}`)
+}
+
+let fruits = ["mango", "jack","appkle","orange"]
+
+for (let i = 0; i < fruits.length; i++){
+    console.log("khabo ",fruits[0])
+
+}
+
+
+let i = 0;
+
+while (i<fruits.length) {
+    console.log("khabo ",fruits[0]);
+
+    //eita na dile computer hang hoye jabe
+    i++
+}
