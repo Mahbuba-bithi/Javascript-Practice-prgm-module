678b1a8612c0c7fe73d9d78d6b17077762a98611
Mahbuba Bithi <mahbubabithi2393@gmail.com>
2026-08-25 23:40:32 -0700

    array done

--- module.2/array.js
+++ module.2/array.js
@@ -1,4 +1,31 @@
 //array
 
-let person = [];
+let person = ["bithi","chaity","fabi","shormy"];
+
+let number = [100,200,300, 250,300,]
+
+
+//console.log(person[2],number[2])
+
+person.push ("rakib")
+person.push ("shuhag")
+
+//last element delete 
+person.pop()
+
+
+//1st e unshift kore 0 number index e direct 
+person.unshift("shuhag")
+//1st theke delete 
+person.shift();
+
+
+//splice 
+//majher ta delete //2 ta delete korte chai 2 
+person.splice(1,2,)
+
+ let result = person.slice(1, 3);
+
+
 console.log(person)
+console.log(result)
