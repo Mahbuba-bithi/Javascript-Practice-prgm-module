08c1c4b21d0cefcca35a7fe4942af0930b96623d
Mahbuba Bithi <mahbubabithi2393@gmail.com>
2026-08-27 22:26:24 +0600

    dry.js done

---
+++ dry.js
@@ -0,0 +1,40 @@
+//we will buil a order prccessing system to understand dry and how important a function is. 
+
+ //function isValidPrice(price){
+     //return typeof price === "number" && price>0;
+ //}
+
+  // jeta return korbe seta kicchu ektar moddhe rakha lagbe ....
+
+//  let result = isValidPrice(1200);
+// console.log(result)
+
+// function isValidPrice(price){
+//     return typeof price === "number" && price>0;
+// }
+
+//  // eivabe o kora jay console e dekha jabe
+
+ 
+// console.log(isValidPrice(1200))
+
+
+
+
+ function isValidPrice(price){
+     return typeof price === "number" && price>0;
+ }
+
+ function isValidEmail(email){
+    return email.includes("@") && email.includes(".");
+ }
+
+ function calculateDiscount(price,discountParcent){
+    if(!isValidPrice(price)){
+        return0;
+    }
+    let discountAmount = price 
+ }
+
+
+ //parle korba eita  abar
