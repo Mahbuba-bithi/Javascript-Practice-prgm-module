4c91bb799a941fa7c947588cec52a941d5060669
Mahbuba Bithi <mahbubabithi2393@gmail.com>
2026-08-28 14:03:58 +0600

    literarls done

---
+++ module.3/literarls.js
@@ -0,0 +1,16 @@
+//math expression
+
+let price = 500;
+let quantity = 3;
+
+console.log(`Total price:${price*quantity} taka\n pay the price`);
+;
+
+//function call
+
+function getDiscount(price){
+    return price*0.1;
+
+}
+
+console.log(`you saved:${getDiscount(price)} taka \nUse this is an investment`)
