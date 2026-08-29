2d17a71a85f24ad28b14e1d30915e1cfa887e940
Mahbuba Bithi <mahbubabithi2393@gmail.com>
2026-08-29 14:42:41 +0600

    for each map done

---
+++ module.3/arrayOps.js
@@ -0,0 +1,25 @@
+//for of er bodole
+//for each can not return
+//map re return new array
+
+// let fruits = ["apple","mang0","lichi"]
+// let newFruits = fruits.forEach((f,indx) => {
+//     // console.log(`${indx} ->${f}`)
+
+// })
+
+
+
+// let fruits = ["apple","mang0","lichi"]
+// let newFruits = fruits.map((f) => f.toUpperCase()); 
+
+// console.log(newFruits)
+
+let fruits = ["apple","mang0","lichi","watermelon"]
+
+let customFruits = fruits.filter((f) => f.length>5 );
+
+console.log(customFruits);
+
+   
+
