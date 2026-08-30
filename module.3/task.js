d97f21f4963131f47d6ddbe616f3e95dff39cc8e
Mahbuba Bithi <mahbubabithi2393@gmail.com>
2026-08-30 12:25:29 +0600

    module 3 done

---
+++ module.3/task.js
@@ -0,0 +1,31 @@
+let movies = [
+  { title: "Inception", year: 2010, rating: 8.8, genre: "Sci-Fi" },
+  { title: "Titanic", year: 1997, rating: 7.9, genre: "Romance" },
+  { title: "The Matrix", year: 1999, rating: 8.7, genre: "Sci-Fi" },
+  { title: "Avatar", year: 2009, rating: 7.8, genre: "Sci-Fi" },
+  { title: "Notebook", year: 2004, rating: 7.8, genre: "Romance" },
+];
+
+//shudhu sci-fi movie, ratting onujayi boro theke chuto sajanu
+
+let topSciFi = movies
+?.filter((m)=>m.genre=="Sci-Fi")
+
+?.sort((a,b)=>b.rating-a.rating)
+?.map((m)=>`${m.title}, rating:${m.rating}`);
+
+console.log(topSciFi)
+
+
+
+
+let products = [
+  { id: 1, title: "Mouse", price: 500, category: "Accessories", inStock: true },
+  { id: 2, title: "Keyboard", price: 1200, category: "Accessories", inStock: false },
+  { id: 3, title: "Monitor", price: 8000, category: "Display", inStock: true },
+  { id: 4, title: "Laptop", price: 55000, category: "Computer", inStock: true },
+  { id: 5, title: "Headphone", price: 1500, category: "Accessories", inStock: true }
+];
+
+// শুধু In-Stock Product, Price অনুযায়ী কম থেকে বেশি সাজানো, শুধু Title+Price বের করা,
+// টোটাল কত টাকা লাগবে স্টক আউট করতে চাইলে সেটা ক্যালকুলেট করুন।
